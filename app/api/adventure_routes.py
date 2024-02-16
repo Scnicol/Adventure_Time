7741529117560c5216965229339ed415b784346d
Scot Nicol <scnicol@ucdavis.edu>
2024-02-16 10:42:12 -0800

    added Get all adventures by current user route

--- app/api/adventure_routes.py
+++ app/api/adventure_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, session, request
-from app.models import Adventure, db
+from app.models import Adventure, User, db
 from app.forms import adventure_form
 from flask_login import login_required, current_user
 
@@ -11,3 +11,15 @@ adventure_routes = Blueprint('adventures', __name__)
 def get_all_adventures():
     adventures = Adventure.query.all()
     return {'adventures': [adventure.to_dict_full() for adventure in adventures]}
+
+#Get all adventures made by the current user
+@adventure_routes.route('/user/current', methods=['GET'])
+@login_required
+def get_user_adventures():
+    currentUserId = current_user.get_id()
+    user = User.query.get(currentUserId)
+
+    if user is None:
+        return {'error': 'User not found'}, 401
+
+    return {'adventures': [adventure.to_dict_full() for adventure in user.adventures]}
