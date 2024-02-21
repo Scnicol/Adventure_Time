8f11756a2aeadbbca1e20b94157e66f61459c99f
Scot Nicol <scnicol@ucdavis.edu>
2024-02-21 10:51:02 -0800

    added GET all activities route

--- app/api/activity_routes.py
+++ app/api/activity_routes.py
@@ -8,4 +8,8 @@ from flask_login import login_required, current_user
 
 activity_routes = Blueprint('activities', __name__)
 
-
+#GET all activities
+@activity_routes.route('', methods=['GET'])
+def get_all_activities():
+    activities = Activity.query.all()
+    return {'activities': [activity.to_dict() for activity in activities]}
