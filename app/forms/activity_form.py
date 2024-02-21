beced54b5381fb5dcb7e1d24871f3458de33828e
Scot Nicol <scnicol@ucdavis.edu>
2024-02-21 10:34:34 -0800

    added the activity form

---
+++ app/forms/activity_form.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField, DateField
+from wtforms.validators import DataRequired
+
+class ActivityForm(FlaskForm):
+    activity = StringField('activity', validators=[DataRequired()])
