cea759567e4ff0ed3d334d4bf8b963f8f86d118e
Scot Nicol <scnicol@ucdavis.edu>
2024-03-08 15:38:34 -0800

    added creatorId to seeder file

--- app/seeds/directions.py
+++ app/seeds/directions.py
@@ -4,107 +4,128 @@ from sqlalchemy.sql import text
 def seed_directions():
     direction1 = Direction(
         adventureId = 1,
-        direction = 'follow a yellow car for 2 minutes'
+        direction = 'follow a yellow car for 2 minutes',
+        creatorId = 1
     )
 
     direction2 = Direction(
         adventureId = 1,
-        direction = 'follow a red car for 2 minutes'
+        direction = 'follow a red car for 2 minutes',
+        creatorId = 1
     )
 
     direction3 = Direction(
         adventureId = 1,
-        direction = 'follow a white car for 2 minutes'
+        direction = 'follow a white car for 2 minutes',
+        creatorId = 1
     )
 
     direction4 = Direction(
         adventureId = 1,
-        direction = 'call a friend and ask for directions for 2 minutes'
+        direction = 'call a friend and ask for directions for 2 minutes',
+        creatorId = 1
     )
 
     direction5 = Direction(
         adventureId = 1,
-        direction = 'go right 5 times and then go straight'
+        direction = 'go right 5 times and then go straight',
+        creatorId = 1
     )
 
     direction6 = Direction(
         adventureId = 1,
-        direction = 'go right then left then straight'
+        direction = 'go right then left then straight',
+        creatorId = 2
     )
 
     direction7 = Direction(
         adventureId = 1,
-        direction = 'let the co-pilot steer the directions from their seat for 2 minutes'
+        direction = 'let the co-pilot steer the directions from their seat for 2 minutes',
+        creatorId = 2
     )
 
     direction8 = Direction(
         adventureId = 1,
-        direction = 'follow the sun for 10 minutes'
+        direction = 'follow the sun for 10 minutes',
+        creatorId = 2
     )
 
     direction9 = Direction(
         adventureId = 1,
-        direction = 'follow the moon for 10 minutes'
+        direction = 'follow the moon for 10 minutes',
+        creatorId = 2
     )
 
     direction10 = Direction(
         adventureId = 1,
-        direction = 'roll down window and ask someone for random directions on the street'
+        direction = 'roll down window and ask someone for random directions on the street',
+        creatorId = 2
     )
 
     direction11 = Direction(
         adventureId = 2,
-        direction = 'each person in turn pick a direction at an intersection until everyone has done this once'
+        direction = 'each person in turn pick a direction at an intersection until everyone has done this once',
+        creatorId = 2
     )
 
     direction12 = Direction(
         adventureId = 2,
-        direction = 'drive opposite the sun for 10 minutes'
+        direction = 'drive opposite the sun for 10 minutes',
+        creatorId = 3
     )
 
     direction13 = Direction(
         adventureId = 2,
-        direction = 'drive opposite the moon for 10 minutes'
+        direction = 'drive opposite the moon for 10 minutes',
+        creatorId = 3
     )
 
     direction14 = Direction(
         adventureId = 2,
-        direction = 'look for tallest building in the distance and try to get as close to it for 3 minutes'
+        direction = 'look for tallest building in the distance and try to get as close to it for 3 minutes',
+        creatorId = 3
     )
 
     direction15 = Direction(
         adventureId = 2,
-        direction = 'follow nearest green car'
+        direction = 'follow nearest green car',
+        creatorId = 3
     )
 
     direction16 = Direction(
         adventureId = 2,
-        direction = 'call a friend and ask them to give you a directions for 2 minutes'
+        direction = 'call a friend and ask them to give you a directions for 2 minutes',
+        creatorId = 3
     )
 
     direction17 = Direction(
         adventureId = 2,
-        direction = 'make a wheel with turn left, turn right, go straight on it and spin it for each intersection for 2 minutes'
+        direction = 'make a wheel with turn left, turn right, go straight on it and spin it for each intersection for 2 minutes',
+        creatorId = 1
     )
 
     direction18 = Direction(
         adventureId = 2,
-        direction = 'for 2 minutes if you are at a stop sign go straight, if you are at a light and its red turn left, if its green is turn right'
+        direction = 'for 2 minutes if you are at a stop sign go straight, if you are at a light and its red turn left, if its green is turn right',
+        creatorId = 1
     )
 
     direction19 = Direction(
         adventureId = 2,
-        direction = 'drive straight with music that has lyrics until they sing the name of the song'
+        direction = 'drive straight with music that has lyrics until they sing the name of the song',
+        creatorId = 3
     )
 
     direction20 = Direction(
         adventureId = 2,
-        direction = 'follow the direction the wind is blowing for two minutes'
+        direction = 'follow the direction the wind is blowing for two minutes',
+        creatorId = 1
     )
 
     direction21 = Direction(
         adventureId = 2,
-        direction = 'follow your nose! wherever it goes!'
+        direction = 'follow your nose! wherever it goes!',
+        creatorId = 1
     )
 
     db.session.add(direction1)
