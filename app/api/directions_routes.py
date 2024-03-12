c6b38f69a5b9042eec2edd831754a844be56bf63
Scot Nicol <scnicol@ucdavis.edu>
2024-03-12 12:05:09 -0700

    added the directions routes file

--- app/api/directions_routes.py
+++ app/api/directions_routes.py
@@ -2,4 +2,9 @@ from flask import Blueprint, jsonify, session, request
 from app.models import Direction, User, db
 from app.forms import LoginForm
 from app.forms import SignUpForm
+from .auth_routes import validation_errors_to_error_messages
 from flask_login import current_user, login_user, logout_user, login_required
+
+direction_routes = Blueprint('activities', __name__)
+
+
