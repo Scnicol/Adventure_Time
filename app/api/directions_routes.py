30b7f083cb901b30300f8503ff0f48bccbffa3cd
Scot Nicol <scnicol@ucdavis.edu>
2024-03-13 13:42:17 -0700

    added Get direction by Id

--- app/api/directions_routes.py
+++ app/api/directions_routes.py
@@ -5,7 +5,7 @@ from app.forms import SignUpForm
 from .auth_routes import validation_errors_to_error_messages
 from flask_login import current_user, login_user, logout_user, login_required
 
-direction_routes = Blueprint('activities', __name__)
+direction_routes = Blueprint('directions', __name__)
 
 #GET all directions
 @direction_routes.route('', methods=['GET'])
@@ -13,4 +13,13 @@ def get_all_directions():
     directions = Direction.query.all()
     return {'directions': [direction.to_dict() for direction in directions]}
 
+#GET direction by Id
+@direction_routes.route('/<int:directionId>', method=['GET'])
+def get_direction_by_id(directionId):
+    direction = Direction.query.get(directionId)
+
+    if direction is None:
+        return {'error', 'direction not found'}, 401
+
+    return direction.to_dict()
 
