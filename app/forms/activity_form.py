00faaf885a1417d8f9cea08faa4ffe927a502cc7
Scot Nicol <scnicol@ucdavis.edu>
2024-03-14 11:17:45 -0700

    Removed unneeded forms

--- app/forms/activity_form.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, DateField
-from wtforms.validators import DataRequired
-
-class ActivityForm(FlaskForm):
-    activity = StringField('activity', validators=[DataRequired()])
-    creatorId = IntegerField('creatorId', validators=[DataRequired()])
--- app/forms/direction_form.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, DateField
-from wtforms.validators import DataRequired
-
-class DirectionForm(FlaskForm):
-    direction = StringField('direction', validators=[DataRequired()])
-    creatorId = IntegerField('creatorId', validators=[DataRequired()])
