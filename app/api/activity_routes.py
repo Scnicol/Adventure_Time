b2b9e5b35c84bb5dae540080ab19173fb01a6960
Scot Nicol <scnicol@ucdavis.edu>
2024-03-14 11:10:07 -0700

    Removed unused imports from activity routes

--- app/api/activity_routes.py
+++ app/api/activity_routes.py
@@ -1,11 +1,7 @@
-from flask import Blueprint, jsonify, session, request
-from app.models import User, db, Adventure, Activity
-from app.forms.activity_form import ActivityForm
+from flask import Blueprint
+from app.models import Activity
 from .instructionProvider import InstructionsProvider
-from datetime import datetime
-from sqlalchemy import and_
-from .auth_routes import validation_errors_to_error_messages
-from flask_login import login_required, current_user
+from flask_login import login_required
 
 activity_routes = Blueprint('activities', __name__)
 provider = InstructionsProvider(Activity, 'activity', 'activities')
