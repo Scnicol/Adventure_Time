4d2fbf933c0540bc003c38783a741201bd711fa6
Scot Nicol <scnicol@ucdavis.edu>
2024-03-26 13:03:19 -0700

    Added new relationships to adventureInstructions join table

--- app/models/adventureInstructions.py
+++ app/models/adventureInstructions.py
@@ -2,20 +2,20 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-class AdventureInstructions(db.Model, UserMixin):
-    __instruction_column__ = 'not_set'
+class AdventureInstruction(db.Model, UserMixin):
+    __table_name__ = 'adventure_instructions'
 
     if environment == 'production':
         __table_args__ = {'schema': SCHEMA}
 
     adventureId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('adventures.id')), primary_key=True)
-    instructionId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod(__instruction_column__)), primary_key=True)
+    instructionId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('instructions.id')), primary_key=True)
     isUsed = db.Column(db.Boolean, default=False, nullable=False)
 
     # Relationships _____________________
 
-    adventure = db.relationship('Adventures', foreign_keys='adventures.id', back_populates='activities')
-    activity = db.relationship('Activities', foreign_keys=__instruction_column__, back_populates='adventures')
+    adventure = db.relationship('Adventure', foreign_keys='AdventureInstruction.adventureId', back_populates='instructionAssociations')
+    instruction = db.relationship('Instruction', foreign_keys='AdventureInstruction.instructionId', back_populates='adventures')
 
     # Methods _________________________
 
@@ -25,18 +25,3 @@ class AdventureInstructions(db.Model, UserMixin):
             'isUsed': self.isUsed,
             'instructionId': self.instructionId,
         }
-
-class AdventureActivities(AdventureInstructions):
-    __tablename__ = 'adventure_activities'
-
-    __instruction_column__ = 'activities.id'
-
-class AdventureDirections(AdventureInstructions):
-    __tablename__ = 'adventure_directions'
-
-    __instruction_column__ = 'activities.id'
-
-class AdventureFood(AdventureInstructions):
-    __tablename__ = 'adventure_food'
-
-    __instruction_column__ = 'food.id'
