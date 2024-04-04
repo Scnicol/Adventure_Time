ab2b39b07def02eba3025603803fb29e32692841
Scot Nicol <scnicol@ucdavis.edu>
2024-04-04 12:13:13 -0700

    Renamed one of the columns for better understanding

--- app/models/instructionType.py
+++ app/models/instructionType.py
@@ -9,7 +9,7 @@ class InstructionType(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
 
