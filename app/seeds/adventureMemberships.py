5cce0b5a295441fd73a143b3f5e7bc749677e1c2
Scot Nicol <scnicol@ucdavis.edu>
2024-05-22 10:17:27 -0700

    fixed the adventureMemberships seed function

--- app/seeds/adventureMemberships.py
+++ app/seeds/adventureMemberships.py
@@ -1,18 +1,25 @@
-from app.models import db, AdventureMembership, User, environment, SCHEMA
+from app.models import db, AdventureMembership, environment, SCHEMA
 from .users import *
+from .adventures import *
 from sqlalchemy.sql import text
 
 def seed_adventureMemberships():
-    for adventure in demo.adventures:
-        for member in users.id:
-            adventure.add_member(member)
+    member1 = AdventureMembership(
+        adventure = adventure1, user = marnie, status = "accepted"
+    )
 
+    member2 = AdventureMembership(
+        adventure = adventure1, user = bobbie, status = "declined"
+    )
+
+    db.session.add(member1)
+    db.session.add(member2)
     db.session.commit()
 
 def undo_adventureMemberships():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.{AdventureMembership.__table_name__} RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.{AdventureMembership.__tablename__} RESTART IDENTITY CASCADE;")
     else:
-        db.session.execute(text(f"DELETE FROM {AdventureMembership.__table_name__}"))
+        db.session.execute(text(f"DELETE FROM {AdventureMembership.__tablename__}"))
 
     db.session.commit()
