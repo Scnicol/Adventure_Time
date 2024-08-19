9975acea7c2d767fc661614f8bf1aaa06528b6bb
Scot Nicol <scnicol@ucdavis.edu>
2024-08-18 17:54:36 -0700

    Creating the adventures store

---
+++ react-app/src/store/adventures.js
@@ -0,0 +1,8 @@
+// ___________ACTION_TYPES___________________
+const GET_ADVENTURES = 'adventures/GET_ADVENTURES'
+
+// ______ACTIONS____________
+const actionGetAdventures = (adventures) => ({
+    type: GET_ADVENTURES,
+    adventures
+})
