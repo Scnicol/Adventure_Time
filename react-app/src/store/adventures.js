9bcfa324c63b5674ae047d229be6691026c33d62
Scot Nicol <scnicol@ucdavis.edu>
2024-08-18 18:31:35 -0700

    adding GetAdventureById

--- react-app/src/store/adventures.js
+++ react-app/src/store/adventures.js
@@ -1,8 +1,14 @@
 // ___________ACTION_TYPES___________________
 const GET_ADVENTURES = 'adventures/GET_ADVENTURES'
+const GET_ADVENTURE_BY_ID = 'adventures/GET_ADVENTURE_BY_ID'
 
 // ______ACTIONS____________
 const actionGetAdventures = (adventures) => ({
     type: GET_ADVENTURES,
     adventures
+})
+
+const actionGetAdventureById = (adventure) => ({
+    type: GET_ADVENTURE_BY_ID,
+    adventure
 })
