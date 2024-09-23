c84cba9b6806e2dd43cdc736aab2b0d31721ef12
Scot Nicol <scnicol@ucdavis.edu>
2024-09-23 09:36:26 -0700

    added GetFoodById

--- react-app/src/store/food.js
+++ react-app/src/store/food.js
@@ -1,10 +1,16 @@
 // ______ACTION_TYPES____________
 const GET_FOOD = 'food/GET_FOOD'
+const GET_FOOD_BY_ID = 'food/GET_FOOD_BY_ID'
 
 
 // ______ACTIONS________________
 const actionGetFood = (food) => ({
     type: GET_FOOD,
+    food
+})
+
+const actionGetFoodById = (food) => ({
+    type: GET_FOOD_BY_ID,
     food
 })
 
@@ -16,6 +22,15 @@ export const getFood = () => async dispatch => {
     if (response.ok) {
         const data = await response.json();
         dispatch(actionGetFood(data.food))
+    }
+}
+
+export const getFoodById = (foodId) => async dispatch => {
+    const response = await fetch(`/api/food/${foodId}`)
+
+    if (response.ok) {
+        const food = await response.json();
+        dispatch(actionGetFoodById(food))
     }
 }
 
