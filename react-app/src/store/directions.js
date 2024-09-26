ceade72274f23edc0763442854f3162208b42433
Scot Nicol <scnicol@ucdavis.edu>
2024-09-26 14:55:10 -0700

    Added Thunk Actions to Directions store

--- react-app/src/store/directions.js
+++ react-app/src/store/directions.js
@@ -6,3 +6,13 @@ const actionGetDirections = (directions) => ({
     type: GET_DIRECTIONS,
     directions
 })
+
+//______THUNK_ACTIONS________________
+export const getDirections = () => async dispatch => {
+    const response = await fetch(`api/directions`);
+
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(actionGetDirections(data.directions))
+    }
+}
