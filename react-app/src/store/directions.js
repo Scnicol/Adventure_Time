6cead9f5dba8637a832008fdde4a9b22ad41ab63
Scot Nicol <scnicol@ucdavis.edu>
2024-09-26 14:53:29 -0700

    Added GET directions to store

---
+++ react-app/src/store/directions.js
@@ -0,0 +1,8 @@
+// __________ACTION_TYPES_____________
+const GET_DIRECTIONS = 'directions/GET_DIRECTIONS'
+
+// __________ACTIONS_________________
+const actionGetDirections = (directions) => ({
+    type: GET_DIRECTIONS,
+    directions
+})
